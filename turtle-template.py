bbf954f5a6e3ce0c46ac7381f979d541fd7f15c5
Richard Beale <98286677+rbeale3422@users.noreply.github.com>
2022-01-30 12:37:11 -0600

    Update turtle-template.py

--- turtle-template.py
+++ turtle-template.py
@@ -1,7 +1,14 @@
 import turtle as trtl
 
+# create and setup screen object
 wn = trtl.Screen()
-painter = trtl.Turtle()
+wn.screensize(640,480)
+wn.bgcolor('black')
+wn.title("My Turtle Program")
 
-wn = trtl.Screen()
+# create and setup drawing turtle
+painter = trtl.Turtle()
+painter.color('white')
+painter.shape('turtle')
+
 wn.mainloop()
