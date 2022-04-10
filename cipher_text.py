a6807a60ff5e65dec8d8afcb6c3ee234873f420f
Richard Beale <98286677+rbeale3422@users.noreply.github.com>
2022-04-10 18:44:33 -0500

    Manually adding some files for tomorrow's class

---
+++ cipher_text.py
@@ -0,0 +1,53 @@
+"""GUI application that takes a message and encodes it"""
+import tkinter as tk
+from message_encode import Message # import my class module
+
+def on_submit():
+    """To be run when user submits the form"""
+    message = message_var.get() # get the unencoded message from the input box
+    message = message.lower() # make everything lower case for ease of program
+    # if there is not a message in the input box, exit the function
+    if message == '':
+        return
+    # create a new Message object and use it to encrypt the message
+    my_message = Message(message, 4)
+    encode_var.set(my_message.encrypted_message) # set encoded text to display in the encoded input box
+
+# create window and set its size and characteristics
+root = tk.Tk()
+root.title('Caesar Cipher')
+root.geometry('600x600')
+root.columnconfigure(1,weight=1) # this causes the text input boxes to expand to fill the space
+
+# 1st row for unencoded message
+message_label = tk.Label(
+    root,
+    text='Enter message to encode:',
+    font=('Arial 16 bold'),
+    bg='red',
+    fg='#FF0'
+)
+message_label.grid(row=0,column=0,sticky=(tk.W), padx=25)
+message_var = tk.StringVar(root) # holds string from the unencoded input widget
+message_input = tk.Entry(root,textvariable=message_var)
+message_input.grid(row=0,column=1,sticky=(tk.N,tk.E,tk.W,tk.S),padx=10)
+
+# 2nd row for encoded message
+encoded_label = tk.Label(
+    root,
+    text='Encoded Message:',
+    font=('Arial 16 bold'),
+    bg='green',
+    fg='#FF0'
+)
+encoded_label.grid(row=1,column=0,sticky=(tk.W), padx=25)
+encode_var = tk.StringVar(root)
+encoded_input = tk.Entry(root,textvariable=encode_var)
+encoded_input.grid(row=1,column=1,sticky=(tk.N,tk.E,tk.W,tk.S), padx=10)
+
+# 3rd row for button that initiates the conversion from plain message to encrypted message
+encode_btn = tk.Button(root, text='Encode Text')
+encode_btn.grid(row=2,column=1,sticky=(tk.E),padx=10,pady=10)
+encode_btn.configure(command=on_submit) # calls the on_submit function
+
+root.mainloop() # keeps window open
---
+++ message_encode.py
@@ -0,0 +1,19 @@
+"""Class module that defines a cipher message class called Message"""
+class Message():
+    def __init__(self, original_message, offset):
+        self.original = original_message
+        self.off = offset
+        self.encrypted_message = self.encode()
+    
+    def encode(self):
+        letters = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
+        new_message = ''
+        for c in self.original:
+            if c == ' ' or c == '.' or c == '?' or c == '!':
+                new_message += c
+            else:
+                letter_pos = letters.index(c) + self.off
+                if letter_pos > len(letters) - 1: # index is past the last letter, wrap around
+                    letter_pos -= len(letters)
+                new_message += letters[letter_pos]
+        return new_message
